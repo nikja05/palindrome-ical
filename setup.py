73a50a078a6fb2f113af7995914f4585ec673bbe
Nikolaj Veljkovic <nikjaspamm@gmail.com>
2024-10-06 08:01:34 -0400

    Can import a list of packages

---
+++ setup.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+class Setup:
+    def __init__(self):
+        pass
+    
+    def install_packages(self, pckg_list):
+        for pckg in pckg_list:
+            install(pckg)
+    
+    def install(self, package):
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        
